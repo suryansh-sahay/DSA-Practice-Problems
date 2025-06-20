92ca28cea0501fd9b4994ad8ed0561842e7431d6
suryansh-sahay <142234917+suryansh-sahay@users.noreply.github.com>
2025-06-20 20:06:57 +0530

    Time: 3 ms (21.09%) | Memory: 21.9 MB (38.84%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -3,23 +3,33 @@ public:
     int missingNumber(vector<int>& nums) {
         
         int n=nums.size();
-        int total =n*(n+1)/2;
-        int sum=0;
-        for(int i=0; i<n; i++){
-            sum+=nums[i];
+        sort(nums.begin(), nums.end());
+        int left=0, right=n;
+        while(left<right){
+           int mid=left+(right-left)/2;
+           if(nums[mid]>mid) right=mid;
+           else{left=mid+1;}
         }
-        return (total-sum);
+        return right;
 
-        // vector<bool> check(n+1, false);
+
+        
+        // int sum=n;
         // for(int i=0; i<n; i++){
-        //     check[nums[i]]=true;
+        //     sum=sum+i-nums[i];
         // }
+        // return sum;
+
+        // // vector<bool> check(n+1, false);
+        // // for(int i=0; i<n; i++){
+        // //     check[nums[i]]=true;
+        // // }
         
-        // int val;
-        // for(int i=0; i<n+1; i++){
-        //     if(check[i]==false) {val=i; break;}
-        // }
+        // // int val;
+        // // for(int i=0; i<n+1; i++){
+        // //     if(check[i]==false) {val=i; break;}
+        // // }
         
-        // return val;
+        // // return val;
     }
 };
