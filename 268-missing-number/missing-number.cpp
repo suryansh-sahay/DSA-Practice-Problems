31364fd9b55c2cd772f269066d1311d591b99c57
suryansh-sahay <142234917+suryansh-sahay@users.noreply.github.com>
2025-06-21 00:45:46 +0530

    Time: 0 ms (100.00%) | Memory: 21.7 MB (70.30%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -3,33 +3,31 @@ public:
     int missingNumber(vector<int>& nums) {
         
         int n=nums.size();
-        sort(nums.begin(), nums.end());
-        int left=0, right=n;
-        while(left<right){
-           int mid=left+(right-left)/2;
-           if(nums[mid]>mid) right=mid;
-           else{left=mid+1;}
-        }
-        return left;
-
-
-        
-        // int sum=n;
-        // for(int i=0; i<n; i++){
-        //     sum=sum+i-nums[i];
+        // sort(nums.begin(), nums.end());  // nlogn
+        // int left=0, right=n;
+        // while(left<right){
+        //    int mid=left+(right-left)/2;
+        //    if(nums[mid]>mid) right=mid;
+        //    else{left=mid+1;}
         // }
-        // return sum;
+        // return left;
 
-        // // vector<bool> check(n+1, false);
-        // // for(int i=0; i<n; i++){
-        // //     check[nums[i]]=true;
-        // // }
+        int sum=n;
+        for(int i=0; i<n; i++){
+            sum=sum+i-nums[i];
+        }
+        return sum;
+
+        // vector<bool> check(n+1, false);
+        // for(int i=0; i<n; i++){
+        //     check[nums[i]]=true;
+        // }
         
-        // // int val;
-        // // for(int i=0; i<n+1; i++){
-        // //     if(check[i]==false) {val=i; break;}
-        // // }
+        // int val;
+        // for(int i=0; i<n+1; i++){
+        //     if(check[i]==false) {val=i; break;}
+        // }
         
-        // // return val;
+        // return val;
     }
 };
