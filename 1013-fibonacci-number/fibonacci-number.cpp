2fcf73a7aede331907eeca6a05940e6b918008d2
suryansh-sahay <142234917+suryansh-sahay@users.noreply.github.com>
2025-06-29 02:25:45 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (36.48%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -12,18 +12,11 @@
 // };
 
 
-class Solution{
-    public:
-    int fib(int n){
-        if(n==0) return 0;
-        if(n==1) return 1;
-        int ans=1, temp1, temp2=0;
-        for(int i=2; i<=n; i++){  
-            temp1=ans; 
-            ans=temp1+temp2; 
-            temp2=temp1;
-        }
-        return ans;
-
+class Solution {
+public:
+    int fib(int n) {
+        double sqrt5 = sqrt(5);
+        double fibN = pow((1 + sqrt5) / 2, n) - pow((1 - sqrt5) / 2, n);
+        return round(fibN / sqrt5);
     }
 };
