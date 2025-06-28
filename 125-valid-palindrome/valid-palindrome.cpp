15940f02c215f0799f25be325f73d028c0dbb25f
suryansh-sahay <142234917+suryansh-sahay@users.noreply.github.com>
2025-06-29 00:58:37 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (93.95%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,19 +1,41 @@
 class Solution {
 private:
     bool isPali(const string& s, int l, int r) {
-        if (l >= r)
-            return true;
-        if (!isalnum(s[l]))
-            return isPali(s, l+1, r);
-        else if (!isalnum(s[r]))
-           return  isPali(s, l, r-1);
-        else if (tolower(s[l]) != tolower(s[r]))
-            return false;
-        else {
-           return isPali(s, l+1, r-1);
-        }
+        while (l < r && !isalnum(s[l])) ++l;
+        while (l < r && !isalnum(s[r])) --r;
+        
+        if (l >= r) return true;
+
+        char left = tolower(s[l]);
+        char right = tolower(s[r]);
+
+        if (left != right) return false;
+
+        return isPali(s, l + 1, r - 1);
     }
 
 public:
-    bool isPalindrome(string s) { return isPali(s, 0, s.size()-1); }
+    bool isPalindrome(string s) {
+        return isPali(s, 0, s.size() - 1);
+    }
 };
+
+// class Solution {
+// private:
+//     bool isPali(const string& s, int l, int r) {
+//         if (l >= r)
+//             return true;
+//         if (!isalnum(s[l]))
+//             return isPali(s, l+1, r);
+//         else if (!isalnum(s[r]))
+//            return  isPali(s, l, r-1);
+//         else if (tolower(s[l]) != tolower(s[r]))
+//             return false;
+//         else {
+//            return isPali(s, l+1, r-1);
+//         }
+//     }
+
+// public:
+//     bool isPalindrome(string s) { return isPali(s, 0, s.size()-1); }
+// };
