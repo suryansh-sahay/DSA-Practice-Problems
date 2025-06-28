4d3ffd9b6ccfc1bad33fd98d922f78c69312d7f1
suryansh-sahay <142234917+suryansh-sahay@users.noreply.github.com>
2025-06-29 00:46:06 +0530

    Time: 4 ms (17.05%) | Memory: 10 MB (42.81%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,24 +1,19 @@
 class Solution {
-public:
-    bool isPalindrome(string s) {
-        int n = s.length();
-        int l = 0, r = n - 1;
-        while (l < r) {
-            if (!isalnum(s[l]) && !isalnum(s[r]))
-                l++, r--;
-            else if (!isalnum(s[l]))
-                l++;
-            else if (!isalnum(s[r]))
-                r--;
-            else {
-                if (tolower(s[l]) != tolower(s[r]))
-                    return false;
-                else {
-                    l++, r--;
-                }
-            }
+private:
+    bool isPali(const string& s, int l, int r) {
+        if (l >= r)
+            return true;
+        if (!isalnum(s[l]))
+            return isPali(s, l+1, r);
+        else if (!isalnum(s[r]))
+           return  isPali(s, l, r-1);
+        else if (tolower(s[l]) != tolower(s[r]))
+            return false;
+        else {
+           return isPali(s, l+1, r-1);
         }
-
-        return true;
     }
+
+public:
+    bool isPalindrome(string s) { return isPali(s, 0, s.size()-1); }
 };
