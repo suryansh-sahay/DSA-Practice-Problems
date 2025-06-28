3669735b9065c61aa1d10fbfbc3d3c04a0bc5fa4
suryansh-sahay <142234917+suryansh-sahay@users.noreply.github.com>
2025-06-29 02:01:09 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (36.48%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,12 +1,29 @@
-class Solution {
-    private:
-    int fibo(int n){
+// class Solution {
+//     private:
+//     int fibo(int n){
+//         if(n==0) return 0;
+//         if(n==1) return 1;
+//         return fibo(n-1)+fibo(n-2);
+//     }
+// public:
+//     int fib(int n) {
+//         return fibo(n);
+//     }
+// };
+
+
+class Solution{
+    public:
+    int fib(int n){
         if(n==0) return 0;
         if(n==1) return 1;
-        return fibo(n-1)+fibo(n-2);
-    }
-public:
-    int fib(int n) {
-        return fibo(n);
+        int ans=1, temp1, temp2=0;
+        for(int i=2; i<=n; i++){  
+            temp1=ans; 
+            ans=temp1+temp2; 
+            temp2=temp1;
+        }
+        return ans;
+
     }
 };
