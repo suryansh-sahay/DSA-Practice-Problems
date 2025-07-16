a27799a2d2b2915fa8c265f9c749138068118e1f
suryansh-sahay <142234917+suryansh-sahay@users.noreply.github.com>
2025-07-16 14:42:24 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (52.34%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -4,11 +4,10 @@ public:
         int n=nums.size();
         int count=0;
         for(int i=0; i<n; i++){
-           if(nums[i]==0) count++;
-           else nums[i-count] =nums[i];
-        }
-        for(int i=n-count; i<n; i++){
-            nums[i]=0;
+          if(nums[i]!=0) {
+            swap(nums[i], nums[count]);
+            count++;
+          }
         }
     }
 };
