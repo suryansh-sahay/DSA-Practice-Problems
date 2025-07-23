773774ae79d180a14b5fd8d20ee77cc85634662f
suryansh-sahay <142234917+suryansh-sahay@users.noreply.github.com>
2025-07-24 00:46:43 +0530

    Time: 136 ms (7.07%) | Memory: 65.7 MB (5.14%) - LeetSync

---
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
@@ -0,0 +1,73 @@
+class Solution {
+public:
+    void clear(stack<pair<char, int>>& st){
+        while(!st.empty()) st.pop();
+    }
+
+    int maximumGain(string s, int x, int y) {
+        int ans = 0;
+        int n = s.length();
+        
+        if (y > x) {
+            stack<pair<char, int>> st;
+            vector<int> vis(n);
+
+            // Prioritize "ba" pairs
+            for (int i = 0; i < s.length(); i++) {
+                if (!st.empty() && st.top().first == 'b' && s[i] == 'a') {
+                    vis[i] = 1;
+                    vis[st.top().second] = 1;
+                    st.pop();
+                    ans += y;
+                } else {
+                    st.push({s[i], i});
+                }
+            }
+
+            clear(st);
+
+            // Process remaining "ab" pairs
+            for (int i = 0; i < n; i++) {
+                if (vis[i] == 1) continue;
+
+                if (!st.empty() && st.top().first == 'a' && s[i] == 'b') {
+                    st.pop();
+                    ans += x;
+                } else {
+                    st.push({s[i], i});
+                }
+            }
+        } else {
+            stack<pair<char, int>> st;
+            vector<int> vis(n);
+
+            // Prioritize "ab" pairs
+            for (int i = 0; i < s.length(); i++) {
+                if (!st.empty() && st.top().first == 'a' && s[i] == 'b') {
+                    vis[i] = 1;
+                    vis[st.top().second] = 1;
+                    st.pop();
+                    ans += x;
+                } else {
+                    st.push({s[i], i});
+                }
+            }
+
+            clear(st);
+
+            // Process remaining "ba" pairs
+            for (int i = 0; i < n; i++) {
+                if (vis[i] == 1) continue;
+
+                if (!st.empty() && st.top().first == 'b' && s[i] == 'a') {
+                    st.pop();
+                    ans += y;
+                } else {
+                    st.push({s[i], i});
+                }
+            }
+        }
+
+        return ans;
+    }
+};
