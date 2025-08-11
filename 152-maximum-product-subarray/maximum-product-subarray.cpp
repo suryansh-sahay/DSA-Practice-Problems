d80ff79164d564e0aa3cc08aee793d226dcc0b47
suryansh-sahay <142234917+suryansh-sahay@users.noreply.github.com>
2025-08-12 02:22:44 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (56.45%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int prefix = 1,suffix =1;
-        int n=nums.size();
-        int currMax = INT_MIN;
-        for(int i=0;i<n;i++){
-            if(prefix==0){
-                prefix = 1;
-            }
-            if(suffix==0){
-                suffix = 1;
-            }
+        int sum1 = 1, sum2 = 1, sum3 = 1, sum4 = 1; 
+        int n = nums.size();
+        int maxi = INT_MIN;
 
-            prefix *= nums[i];
+        for (int i = 0; i < n; i++) {
+            sum1 = max(sum1 * nums[i], nums[i]);
+            sum2 *= nums[i];
 
-            int fromEnd = n - i - 1;
+            sum3 = max(sum3 * nums[n - 1 - i], nums[n - 1 - i]);
+            sum4 *= nums[n - 1 - i];
 
-            suffix *= nums[fromEnd];
-
-            
-
-            currMax = max(currMax,max(suffix,prefix));
-
-
-
+            int temp1 = max(sum1, sum2);
+            int temp2 = max(sum3, sum4);
+            int temp = max(temp1, temp2);
+            maxi = max(maxi, temp);
+            if (sum2 == 0) sum2 = 1; 
+            if (sum4 == 0) sum4 = 1; 
         }
-        return currMax;
+
+        return maxi;
     }
 };
