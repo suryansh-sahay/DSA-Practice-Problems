32401f24c455e448424f04884d218677f3c89444
suryansh-sahay <142234917+suryansh-sahay@users.noreply.github.com>
2025-08-15 15:38:37 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (52.22%) - LeetSync

--- 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.cpp
+++ 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.cpp
@@ -5,6 +5,7 @@ public:
         for(int i=0; i<num.size()-2; i++){
             if(num[i]==num[i+1]&& num[i]==num[i+2]){
                answer= max(answer, num.substr(i,3));
+               i++;
             }
         }
         return answer;
