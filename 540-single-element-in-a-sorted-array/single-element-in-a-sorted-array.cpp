a92def1eaa7a637ae49db25cbd55945c7dfd4c17
suryansh-sahay <142234917+suryansh-sahay@users.noreply.github.com>
2025-08-24 02:36:35 +0530

    Time: 0 ms (100.00%) | Memory: 26.3 MB (7.45%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -6,7 +6,7 @@ public:
         while(l<r){
             int mid=l+(r-l)/2;
             if(mid%2==0) mid--;
-            if(mid==0 || nums[mid]!=nums[mid-1]) r=mid-1;
+            if(nums[mid]!=nums[mid-1]) r=mid-1;
             else l=mid+1;
         }
         return nums[l];
