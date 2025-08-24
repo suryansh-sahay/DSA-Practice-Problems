7e70d76436a8f76461d430ebe5deab2d1622f034
suryansh-sahay <142234917+suryansh-sahay@users.noreply.github.com>
2025-08-24 20:44:42 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (7.31%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -2,5 +2,12 @@ class Solution {
 public:
     double myPow(double x, int n) {
         return pow(x,n);
+        
+        double ans=1;
+        for(int i=1; i<=n; i++){
+           if(n<0) ans=1/(ans*x);
+           else ans*=x;
+        }
+        return ans;
     }
 };
