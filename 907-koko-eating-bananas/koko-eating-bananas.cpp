dd1c1fff89a0aa6280371177765bfe39cbb94eb0
suryansh-sahay <142234917+suryansh-sahay@users.noreply.github.com>
2025-08-27 05:15:21 +0530

    Time: 15 ms (21.38%) | Memory: 22.9 MB (42.13%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,39 +1,19 @@
 class Solution {
 public:
-    // Helper function: checks if Koko can eat all bananas
-    // at speed k within h hours
-    bool canFinish(const vector<int>& piles, int h, int k) {
-        long long hours = 0;  // use long long to avoid overflow
-        for (int pile : piles) {
-            // Time to eat this pile = ceil(pile / k)
-            hours += pile / k;
-            if (pile % k != 0) hours++;  // add 1 if not divisible exactly
-
-            // Optimization: stop early if already exceeding h
-            if (hours > h) return false;
-        }
-        return hours <= h;
-    }
-
-    // Main function: binary search for minimum eating speed
-    int minEatingSpeed(vector<int>& piles, int h) {
-        int left = 1;  // lowest possible speed
-        int right = *max_element(piles.begin(), piles.end()); // highest possible speed
-        int ans = right; // worst case: eat max pile in 1 hour
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;  // safe mid calculation
-
-            if (canFinish(piles, h, mid)) {
-                // If Koko can finish with this speed,
-                // try a smaller speed
-                ans = mid;
-                right = mid - 1;
-            } else {
-                // Otherwise, need to eat faster
-                left = mid + 1;
+    int minEatingSpeed(vector<int>& piles, int h) {     
+        int maxi=-1;
+        for(int num: piles) if(num > maxi) maxi=num;
+        
+        int l=1, r=maxi;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            long long sum=0;
+            for(int num: piles){
+                sum+=ceil((num+mid-1)/mid);
             }
+            if(sum>h) l=mid+1;
+            else r=mid-1;
         }
-        return ans;
+        return l;
     }
 };
