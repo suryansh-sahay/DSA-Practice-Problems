23fa307f0a9620d365024c89d493f92b871bbe23
suryansh-sahay <142234917+suryansh-sahay@users.noreply.github.com>
2025-08-31 02:24:17 +0530

    Time: 0 ms (100.00%) | Memory: 20.2 MB (54.05%) - LeetSync

---
+++ 902-minimum-number-of-refueling-stops/minimum-number-of-refueling-stops.cpp
@@ -0,0 +1,25 @@
+
+class Solution {
+public:
+    int minRefuelStops(int target, int startFuel, vector<vector<int>>& stations) {
+        priority_queue<int> maxHeap;  // max-heap for fuels of reachable stations
+        int n = stations.size();
+        int i = 0, stops = 0;
+        long long fuel = startFuel;
+
+        while (fuel < target) {
+            while (i < n && stations[i][0] <= fuel) {
+                maxHeap.push(stations[i][1]);
+                i++;
+            }
+
+            if (maxHeap.empty()) return -1;
+
+            fuel += maxHeap.top();
+            maxHeap.pop();
+            stops++;
+        }
+
+        return stops;
+    }
+};
