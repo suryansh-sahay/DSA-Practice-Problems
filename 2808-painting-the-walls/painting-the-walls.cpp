28944cd60549558781110a424d4489ceedb64d36
suryansh-sahay <142234917+suryansh-sahay@users.noreply.github.com>
2025-08-30 06:38:20 +0530

    Time: 19 ms (93.76%) | Memory: 95.6 MB (87.16%) - LeetSync

--- 2808-painting-the-walls/painting-the-walls.cpp
+++ 2808-painting-the-walls/painting-the-walls.cpp
@@ -1,26 +1,18 @@
 class Solution {
-    int solve(vector<int> &c, vector<int> &t, int &n, int i, int x, unordered_map<int,int> *mp) {
-        if(x>=n-i) {
-            return 0;
-        }
-        if(i==n) {
-            if(x<0) {
-                return 1e9;
-            }
-            return 0;
-        }
-        if(mp[i].count(x)) {
-            return mp[i][x];
-        }
-        int f = solve(c,t,n,i+1,x-1,mp);
-        int p = c[i] + solve(c,t,n,i+1,x+t[i],mp);
-        return mp[i][x] = min(f,p);
-    }
 public:
     int paintWalls(vector<int>& cost, vector<int>& time) {
         int n = cost.size();
-        unordered_map<int,int> dp[n];
-        return solve(cost,time,n,0,0,dp);
+        const int INF = 1e9;
+        vector<int> dp(n + 1, INF);
+        dp[0] = 0;  // Base case: cost to paint 0 walls is 0
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = n; j >= 0; --j) {
+                int k = min(n, j + time[i] + 1); // how many walls this paid painter can cover
+                dp[k] = min(dp[k], dp[j] + cost[i]);
+            }
+        }
+
+        return dp[n];
     }
 };
-
