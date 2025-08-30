66b5a070ba6ce34416cfe93d696bf492d584b71a
suryansh-sahay <142234917+suryansh-sahay@users.noreply.github.com>
2025-08-30 06:00:20 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (23.87%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
-    bool isValid(vector<int>& nums, int k, int mid) {
-        int cnt = 1, sum = 0;
-        for(int i = 0; i < nums.size(); i++) {
-            if(sum+nums[i] > mid) {
-                cnt++;
-                sum = 0;
-            }
-            sum += nums[i];
-        }
-        return k >= cnt;
-    }
     int splitArray(vector<int>& nums, int k) {
-        int l = nums[0], r = nums[0];
-        for(int i = 1; i < nums.size(); i++) {
-            l = max(l, nums[i]);
-            r += nums[i];
-        }
-        while(l < r) {
-            int mid = l + (r-l)/2;
-            if(isValid(nums, k, mid))
-                r = mid;
-            else
-                l = mid + 1;
+        int l=*max_element(nums.begin(), nums.end());
+        int r=accumulate(nums.begin(), nums.end(), 0);
+
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            int sum=0, count=1;
+            
+            for(int num: nums){
+                if(sum+num>mid) count++, sum=0;
+                sum+=num;
+            }
+            if(count>k) l=mid+1;
+            else r=mid-1;
         }
         return l;
     }
