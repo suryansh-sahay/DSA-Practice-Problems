72aaeebf493d3492c0b1accd795595e7ff71345b
suryansh-sahay <142234917+suryansh-sahay@users.noreply.github.com>
2025-09-11 04:27:48 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (84.86%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -4,12 +4,9 @@ public:
         string ans=strs[0];
         for(int i=1; i<strs.size(); i++){
             int j=0;
-            string temp="";
-            for(char c: strs[i]){
-                if(c==ans[j]) temp+=c, j++;
-                else break;
-            }
-            ans=temp;
+            while(j<ans.size()&&j<strs[i].size() &&ans[j]==strs[i][j]) j++;
+            ans=ans.substr(0, j);
+            if(ans.empty()) return "";
         }    
         return ans;    
     }
