3e27a20e0560ac27065ef0a8be84a0d8dc50aebc
suryansh-sahay <142234917+suryansh-sahay@users.noreply.github.com>
2025-09-12 02:22:57 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (81.62%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,33 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
 class Solution {
 public:
-    int myAtoi(string s) 
-    {
-        int i=0;
-        int sign=1;
-        long ans=0;
-        while(i<s.length() && s[i]==' ')
-            i++;
-        if(s[i]=='-')
-        {
-            sign=-1;
+    int myAtoi(string s) {
+        int i = 0, sign = 1;
+        long res = 0; // Using long to handle overflow cases
+
+        // Trim leading spaces
+        while (i < s.size() && s[i] == ' ') i++;
+        if (i == s.size()) return 0;
+
+        // Check for sign
+        if (s[i] == '-') { sign = -1; i++; }
+        else if (s[i] == '+') i++;
+
+        // Process numerical characters
+        while (i < s.size() && isdigit(s[i])) {
+            res = res * 10 + (s[i] - '0');
+
+            // Handle overflow
+            if (sign * res > INT_MAX) return INT_MAX;
+            if (sign * res < INT_MIN) return INT_MIN;
+
             i++;
         }
-        else if(s[i]=='+')
-            i++;
-        while(i<s.length())
-        {
-            if(s[i]>='0' && s[i]<='9')
-            {
-                ans=ans*10+(s[i]-'0');
-                if(ans>INT_MAX && sign==-1)
-                    return INT_MIN;
-                else if(ans>INT_MAX && sign==1)
-                    return INT_MAX;
-                i++;
-            }
-            else
-                return ans*sign;
-        }
-        return (ans*sign);
+
+        return (int)(sign * res);
     }
 };
