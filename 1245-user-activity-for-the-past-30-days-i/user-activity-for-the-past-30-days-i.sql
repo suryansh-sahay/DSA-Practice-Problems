487607ad78b63b5d22580c146a7cf74b457cdd26
suryansh-sahay <142234917+suryansh-sahay@users.noreply.github.com>
2025-09-21 02:35:31 +0530

    Time: 498 ms (46.85%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+
+SELECT activity_date AS day, COUNT(Distinct user_id) as active_users
+FROM Activity
+WHERE activity_date BETWEEN DATE_SUB('2019-07-28', INTERVAL 1 Month) 
+                    AND  '2019-07-27'
+GROUP BY activity_date;
