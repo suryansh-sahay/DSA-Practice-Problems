c47802f2b8c19b6b771ec0364a7402ba2fc981b5
suryansh-sahay <142234917+suryansh-sahay@users.noreply.github.com>
2025-09-21 03:36:02 +0530

    Time: 644 ms (7.74%) | Memory: 0B (100.00%) - LeetSync

--- 619-biggest-single-number/biggest-single-number.sql
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 
 SELECT MAX(num) as num
-FROM MyNumbers
-WHERE num IN (SELECT num FROM MyNumbers 
-        GROUP BY num HAVING COUNT(num)<2);
+FROM (SELECT num FROM MyNumbers 
+        GROUP BY num HAVING COUNT(num)<2) as any;
+
