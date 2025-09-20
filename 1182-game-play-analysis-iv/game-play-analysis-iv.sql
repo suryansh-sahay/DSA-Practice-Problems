7c7885bf7d3ada0d38d3297700791b287f44b7c7
suryansh-sahay <142234917+suryansh-sahay@users.noreply.github.com>
2025-09-20 20:17:29 +0530

    Time: 567 ms (54.43%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -2,8 +2,8 @@
 
 SELECT ROUND(COUNT(player_id)/(SELECT count(DISTINCT player_id) FROM Activity),2) as fraction
 FROM Activity
-WHERE (player_id, event_date) IN (
-     SELECT player_id, DATE_ADD(MIN(event_date), INTERVAL 1 DAY )
+WHERE (player_id, DATE_SUB(event_date, INTERVAL 1 DAY)) IN (
+     SELECT player_id, MIN(event_date) 
      FROM Activity 
     GROUP BY player_id
     ); 
