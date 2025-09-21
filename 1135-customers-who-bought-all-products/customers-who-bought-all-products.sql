6960dc001a63b55d9deeb5b935a26bc73fef935d
suryansh-sahay <142234917+suryansh-sahay@users.noreply.github.com>
2025-09-21 16:17:52 +0530

    Time: 611 ms (31.32%) | Memory: 0B (100.00%) - LeetSync

--- 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
+++ 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -7,4 +7,4 @@
 SELECT customer_id 
 FROM Customer
 GROUP BY customer_id
-HAVING count(distinct product_key)=(SELECT count( distinct product_key) FROM Product);
+HAVING count(distinct product_key)=(SELECT count(product_key) FROM Product);
