bcbd29d0837a5b6dc9221c4dd958cb9e962fd916
suryansh-sahay <142234917+suryansh-sahay@users.noreply.github.com>
2025-09-23 02:34:09 +0530

    Time: 482 ms (54.20%) | Memory: 0B (100.00%) - LeetSync

--- 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -1,30 +1,13 @@
--- Write your PostgreSQL query statement below
-select
-    distinct product_id,
-    10 as price
-from
-    Products
-group by
-    product_id
-having
-    min(change_date) > '2019-08-16'
-
-union
-
-select
+/* Write your T-SQL query statement below */
+SELECT
     product_id,
-    new_price as price
-from
-    Products
-where
-    (product_id, change_date) in (
-        select
-            product_id,
-            max(change_date)
-        from
-            Products
-        where
-            change_date <= '2019-08-16'
-        group by
-            product_id
-        )
+    FIRST_VALUE(new_price) OVER (PARTITION BY product_id ORDER BY change_date DESC) AS price
+FROM Products
+WHERE change_date <= '2019-08-16'
+UNION
+SELECT
+    product_id,
+    10 AS price
+FROM Products
+GROUP BY product_id
+HAVING MIN(change_date) > '2019-08-16'
