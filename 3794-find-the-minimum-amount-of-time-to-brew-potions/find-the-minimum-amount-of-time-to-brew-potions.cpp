45ce6c02b15a6c651e3e1e7ed754ef9df33ac830
suryansh-sahay <142234917+suryansh-sahay@users.noreply.github.com>
2025-10-10 03:53:09 +0530

    Time: 79 ms (98.46%) | Memory: 44.4 MB (100.00%) - LeetSync

--- 3794-find-the-minimum-amount-of-time-to-brew-potions/find-the-minimum-amount-of-time-to-brew-potions.cpp
+++ 3794-find-the-minimum-amount-of-time-to-brew-potions/find-the-minimum-amount-of-time-to-brew-potions.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
-    long long minTime(vector<int>& skill, vector<int>& mana) {
-        int n = skill.size(), m = mana.size();
-        vector<long long> done(n + 1, 0);
-        
-        for (int j = 0; j < m; ++j) {
-            for (int i = 0; i < n; ++i) {
-                done[i + 1] = max(done[i + 1], done[i]) + 1LL * mana[j] * skill[i];
-            }
-            for (int i = n - 1; i > 0; --i) {
-                done[i] = done[i + 1] - 1LL * mana[j] * skill[i];
-            }
+    static long long minTime(vector<int>& skill, vector<int>& mana) {
+        const int n=skill.size(), m=mana.size();
+
+        partial_sum(skill.begin(), skill.end(), skill.begin());
+        const int skill0=skill[0];
+        long long start=0;
+
+        for (int j=1; j<m; j++) {
+            const long long curr=mana[j], prev=mana[j-1];
+            long long tMax=skill0*prev;
+            for (int i=1; i<n; i++) 
+                tMax=max(tMax, skill[i]*prev-skill[i-1]*curr);
+            start+=tMax;
         }
-        
-        return done[n];
+        return start+1LL*skill.back()*mana.back();
     }
 };
