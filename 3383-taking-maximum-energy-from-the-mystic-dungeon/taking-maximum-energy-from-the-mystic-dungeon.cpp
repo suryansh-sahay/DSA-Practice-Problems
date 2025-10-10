fbd64100eeef195feb78f6fb1fdfd28d880d5fe0
suryansh-sahay <142234917+suryansh-sahay@users.noreply.github.com>
2025-10-11 04:27:01 +0530

    Time: 67 ms (99.67%) | Memory: 149.3 MB (60.40%) - LeetSync

--- 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.cpp
+++ 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.cpp
@@ -1,13 +1,24 @@
 class Solution {
 public:
     int maximumEnergy(vector<int>& energy, int k) {
-        int n = energy.size();
-        vector<int> dp(n);
-        int result = INT_MIN;
-        for (int i = n - 1; i >= 0; --i) {
-            dp[i] = energy[i] + ((i + k < n) ? dp[i + k] : 0);
-            result = max(result, dp[i]);
+        int n=energy.size();
+        vector<int> dp(k), sum(k);
+        for(int i=n-1; i>=n-k; i--) 
+            sum[i%k]=dp[i%k]=energy[i];
+        
+        for(int i=n-k-1; i>=0; i--){
+            int iModk=i%k;
+            sum[iModk]+=energy[i];
+        //    cout<<i<<"->"<<sum[iModk]<<endl;
+            dp[iModk]=max(dp[iModk], sum[iModk]);
         }
-        return result;
+        return *max_element(dp.begin(), dp.end());
     }
 };
+
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
