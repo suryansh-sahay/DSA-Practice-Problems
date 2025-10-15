24b50ca05c4eeda5d759c9d7873169cb435ac60f
suryansh-sahay <142234917+suryansh-sahay@users.noreply.github.com>
2025-10-16 04:39:41 +0530

    Time: 2 ms (3.85%) | Memory: 8.7 MB (44.20%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,31 +1,24 @@
-
-const size_t BUFFER_SIZE = 0x6fafffff;
-alignas(std::max_align_t) char buffer[BUFFER_SIZE];
-size_t buffer_pos = 0;
-
-void* operator new(size_t size) {
-    constexpr std::size_t alignment = alignof(std::max_align_t);
-    size_t padding = (alignment - (buffer_pos % alignment)) % alignment;
-    size_t total_size = size + padding;
-    char* aligned_ptr = &buffer[buffer_pos + padding];
-    buffer_pos += total_size;
-    return aligned_ptr;
-}
-
-void operator delete(void* ptr, unsigned long) {}
-void operator delete(void* ptr) {}
-void operator delete[](void* ptr) {}
-
 class Solution {
 public:
     double myPow(double x, int n) {
-        return pow(x,n);
         
-        double ans=1;
-        for(int i=1; i<=n; i++){
-           if(n<0) ans=1/(ans*x);
-           else ans*=x;
+        if(n < 0) {
+            x = 1 / x;
+        } 
+        
+        long num = labs(n);
+        
+        double pow = 1;
+        
+        while(num){ 
+            if(num & 1) {
+                pow *= x;
+            }
+            
+            x *= x;
+            num >>= 1;
         }
-        return ans;
+        
+        return pow;
     }
 };
