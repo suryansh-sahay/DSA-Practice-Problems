b611aecea6eb81a48d7af10e47f0629572706963
suryansh-sahay <142234917+suryansh-sahay@users.noreply.github.com>
2025-10-22 04:58:32 +0530

    Time: 7 ms (98.54%) | Memory: 91.6 MB (86.10%) - LeetSync

--- 3622-maximum-frequency-of-an-element-after-performing-operations-i/maximum-frequency-of-an-element-after-performing-operations-i.cpp
+++ 3622-maximum-frequency-of-an-element-after-performing-operations-i/maximum-frequency-of-an-element-after-performing-operations-i.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
-int maxFrequency(vector<int>& nums, int k, int numOperations){
-    int n = nums.size();
-    unordered_map<int,int> freq;
-    freq.reserve(n*2);
-    for(int x: nums) freq[x]++;
-    vector<long long> starts, ends;
-    starts.reserve(n); ends.reserve(n);
-    for(int x: nums){ starts.push_back((long long)x - k); ends.push_back((long long)x + k); }
-    sort(starts.begin(), starts.end());
-    sort(ends.begin(), ends.end());
-    vector<pair<long long,int>> events; events.reserve(2*n);
-    for(int i=0;i<n;i++){ events.emplace_back(starts[i], 1); events.emplace_back(ends[i] + 1, -1); }
-    sort(events.begin(), events.end());
-    int curr = 0, max_cover = 0;
-    for(auto &e: events){ curr += e.second; max_cover = max(max_cover, curr); }
-    int ans = min(max_cover, numOperations);
-    for(auto &p: freq){
-        int v = p.first;
-        int existing = p.second;
-        int starts_le = upper_bound(starts.begin(), starts.end(), v) - starts.begin();
-        int ends_lt = lower_bound(ends.begin(), ends.end(), v) - ends.begin();
-        int cover = starts_le - ends_lt;
-        ans = max(ans, min(cover, existing + numOperations));
+    int maxFrequency(vector<int>& nums, int k, int numOperations) {
+        int minNum = *min_element(nums.begin(), nums.end());
+        int maxNum = *max_element(nums.begin(), nums.end());
+        int freq[maxNum + 1], accumu[maxNum + 1];
+        memset(freq, 0, sizeof(freq));
+        memset(accumu, 0, sizeof(accumu));
+        for (const int num: nums) {
+            ++freq[num];
+        }
+        for (int i = minNum; i <= maxNum; i++) {
+            accumu[i] = accumu[i - 1] + freq[i];
+        }
+        int res = 0;
+        for (int i = minNum; i <= maxNum; i++) {
+            // (l, r]
+            int l = max(0, i - k - 1), r = min(maxNum, i + k);
+            int totalCnt = accumu[r] - accumu[l];
+            int opCnt = totalCnt - freq[i];
+            res = max(res, freq[i] + min(opCnt, numOperations));
+        }
+        return res;
     }
-    return ans;
-}
 };
