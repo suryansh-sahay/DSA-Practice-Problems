c5d59b4b9a02b83ec0222a5c58d89e2cc61aaa0f
suryansh-sahay <142234917+suryansh-sahay@users.noreply.github.com>
2025-10-29 03:07:09 +0530

    Time: 9 ms (30.95%) | Memory: 21.8 MB (94.29%) - LeetSync

--- 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.cpp
+++ 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.cpp
@@ -2,16 +2,14 @@ class Solution {
 public:
     int countValidSelections(vector<int>& nums) {
         int sum=accumulate(nums.begin(), nums.end(), 0);
-             cout<<sum<<endl;
         
         int temp=0, ans=0;
         for(int num: nums){
             temp+=num;
             if(num==0){
                 if(sum==2*temp) ans+=2;
-                else if (abs(sum-2*temp)==1) ans+=1;
+                else if (abs(sum-2*temp)==1) ans++;
             }
-            cout<<temp<<" ";
         }
         return ans;
     }
