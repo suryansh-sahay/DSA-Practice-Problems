bd9714cdf5f7fe8c0a150980399d7a1082ddc558
suryansh-sahay <142234917+suryansh-sahay@users.noreply.github.com>
2025-11-04 05:09:58 +0530

    Time: 0 ms (100.00%) | Memory: 77.1 MB (44.90%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -1,28 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* dummyHead = new ListNode(0);
-        ListNode* tail = dummyHead;
-        int carry = 0;
-
-        while (l1 != nullptr || l2 != nullptr || carry != 0) {
-            int digit1 = (l1 != nullptr) ? l1->val : 0;
-            int digit2 = (l2 != nullptr) ? l2->val : 0;
-
-            int sum = digit1 + digit2 + carry;
-            int digit = sum % 10;
-            carry = sum / 10;
-
-            ListNode* newNode = new ListNode(digit);
-            tail->next = newNode;
-            tail = tail->next;
-
-            l1 = (l1 != nullptr) ? l1->next : nullptr;
-            l2 = (l2 != nullptr) ? l2->next : nullptr;
+        int c = 0;
+        ListNode* dummy = new ListNode(0);
+        ListNode* prev = dummy;
+        while (l1 || l2 || c!=0) {
+            int d1=(l1)? l1->val: 0;
+            int d2=(l2)? l2->val: 0;
+            int d3 = (d1 + d2 + c) % 10;
+            c = (d1 + d2 + c) / 10;
+            ListNode* node = new ListNode(d3);
+            prev->next = node;
+            prev = prev->next;
+            l1=(l1)?l1->next:nullptr;
+            l2=(l2)?l2->next:nullptr;
         }
-
-        ListNode* result = dummyHead->next;
-        delete dummyHead;
-        return result;
+        return dummy->next;
     }
 };
+;
