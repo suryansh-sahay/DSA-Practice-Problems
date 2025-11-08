07dac78ae9f907187e096f4dbdf67b18d51a541c
suryansh-sahay <142234917+suryansh-sahay@users.noreply.github.com>
2025-11-09 03:57:36 +0530

    Time: 4 ms (69.30%) | Memory: 15.2 MB (48.47%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -1,56 +1,19 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    Node* next;
-    Node* random;
-    
-    Node(int _val) {
-        val = _val;
-        next = NULL;
-        random = NULL;
-    }
-};
-*/
-
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        if (!head) return nullptr;
-
-        Node* temp = head;
-        while (temp) {
-            Node* node = new Node(temp->val);
-            node->next = temp->next;
-            temp->next = node;
-            temp = node->next;
+        std:unordered_map<Node*,Node*> hashMap;
+        Node* cur = head;
+        while (cur) {
+            hashMap[cur] = new Node(cur->val);
+            cur = cur->next;
         }
-
-        temp = head;
-        while (temp) {
-            Node* node = temp->next;
-            if (temp->random) {
-                node->random = temp->random->next;
-            } else {
-                node->random = nullptr;
-            }
-            temp = node->next; // Move to the next original node
+        cur = head;
+        while (cur) {
+            Node* copy = hashMap[cur];
+            copy->next = hashMap[cur->next];
+            copy->random = hashMap[cur->random];
+            cur = cur->next;
         }
-
-        Node* newHead = head->next;
-        temp = head;
-        while (temp) {
-            Node* node = temp->next;
-            temp->next = node->next; 
-            
-            if (node->next) {
-                node->next = node->next->next;
-            }
-            
-            temp = temp->next;
-        }
-        
-        return newHead;
+        return hashMap[head];
     }
 };
