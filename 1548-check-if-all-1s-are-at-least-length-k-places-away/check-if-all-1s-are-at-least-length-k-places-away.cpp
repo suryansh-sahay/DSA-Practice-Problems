ffffefc1eba6a80bd8a65b10dc304c16c5917151
suryansh-sahay <142234917+suryansh-sahay@users.noreply.github.com>
2025-11-18 05:27:16 +0530

    Time: 0 ms (100.00%) | Memory: 61.5 MB (13.78%) - LeetSync

--- 1548-check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.cpp
+++ 1548-check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.cpp
@@ -1,12 +1,18 @@
+// Time:  O(n)
+// Space: O(1)
+
 class Solution {
 public:
     bool kLengthApart(vector<int>& nums, int k) {
-        int n2=-1e7;
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i]==1){
-                if(i-n2-1 < k) return false;
-                n2=i;
+        int prev = -k - 1;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (!nums[i]) {
+                continue;
             }
+            if (i - prev <= k) {
+                return false;
+            }
+            prev = i;
         }
         return true;
     }
