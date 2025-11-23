3b15660129779d972dc1355ff65e7260d1a2c328
suryansh-sahay <142234917+suryansh-sahay@users.noreply.github.com>
2025-11-23 05:57:03 +0530

    Time: 3 ms (86.49%) | Memory: 29.2 MB (77.11%) - LeetSync

--- 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.cpp
+++ 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.cpp
@@ -1,35 +1,18 @@
 class Solution {
+    int n;
+    vector<vector<int>> dp;
 public:
     int maxSumDivThree(vector<int>& nums) {
-        int sum = 0;
-        int min1 = INT_MAX, min2 = INT_MAX;
-        int min11 = INT_MAX, min22 = INT_MAX;
-
-        for (int x : nums) {
-            sum += x;
-            int r = x % 3;
-            if (r == 1) {
-                if (x < min1) { min11 = min1; min1 = x; }
-                else if (x < min11) min11 = x;
-            } else if (r == 2) {
-                if (x < min2) { min22 = min2; min2 = x; }
-                else if (x < min22) min22 = x;
+        int n = nums.size();
+        int dp[n + 1][3];
+        dp[0][0] = 1; dp[0][1] = dp[0][2] = 0;
+        for (int i = 1; i <= n; ++i) {
+            for (int m = 0; m < 3; ++m) {
+                int skip = dp[i - 1][m], pick = 0, x = dp[i - 1][(m + nums[i - 1]) % 3];
+                if (x) pick = nums[i - 1] + x;
+                dp[i][m] = max(pick, skip);
             }
         }
-        int rem = sum % 3;
-
-        if (rem == 0) return sum;
-
-        if (rem == 1) {
-            int r1 = min1;
-            int r2 = (min2 == INT_MAX || min22 == INT_MAX) ? INT_MAX : min2 + min22;
-            int remove = min(r1, r2);
-            return remove == INT_MAX ? 0 : sum - remove;
-        } else {
-            int r1 = min2;
-            int r2 = (min1 == INT_MAX || min11 == INT_MAX) ? INT_MAX : min1 + min11;
-            int remove = min(r1, r2);
-            return remove == INT_MAX ? 0 : sum - remove;
-        }
+        return dp[n][0] - 1;
     }
 };
