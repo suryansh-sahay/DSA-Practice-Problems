0bb8b55833398a32a031720c9f43d651a6e10ca4
suryansh-sahay <142234917+suryansh-sahay@users.noreply.github.com>
2025-12-31 01:54:18 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (88.54%) - LeetSync

--- 870-magic-squares-in-grid/magic-squares-in-grid.cpp
+++ 870-magic-squares-in-grid/magic-squares-in-grid.cpp
@@ -1,37 +1,29 @@
 class Solution {
 public:
-    bool isMagicSquare(vector<vector<int>>& grid, int r, int c) {
-        int sum = grid[r][c] + grid[r][c+1] + grid[r][c+2];
-        unordered_set<int> st;
-
-        for(int i = 0; i < 3; i++) {
-            for(int j = 0; j < 3; j++) {
-                int num = grid[r+i][c+j];
-                if(num < 1 || num > 9 || st.count(num)) return false;
-                st.insert(num);
+    bool isMagic(vector<vector<int>>& g, int r, int c) {
+        vector<int> seen(10, 0);
+        for (int i = r; i < r + 3; i++)
+            for (int j = c; j < c + 3; j++) {
+                int v = g[i][j];
+                if (v < 1 || v > 9 || seen[v]) return false;
+                seen[v] = 1;
             }
-        }
 
-        for(int i = 0; i < 3; i++) {
-            if(grid[r][c+i] + grid[r+1][c+i] + grid[r+2][c+i] != sum) return false;
-            if(grid[r+i][c] + grid[r+i][c+1] + grid[r+i][c+2] != sum) return false;
-        }
+        int s = g[r][c] + g[r][c+1] + g[r][c+2];
+        for (int i = 0; i < 3; i++)
+            if (g[r+i][c] + g[r+i][c+1] + g[r+i][c+2] != s) return false;
+        for (int j = 0; j < 3; j++)
+            if (g[r][c+j] + g[r+1][c+j] + g[r+2][c+j] != s) return false;
 
-        if(grid[r][c] + grid[r+1][c+1] + grid[r+2][c+2] != sum) return false;
-        if(grid[r][c+2] + grid[r+1][c+1] + grid[r+2][c] != sum) return false;
-
-        return true;
+        return g[r][c] + g[r+1][c+1] + g[r+2][c+2] == s &&
+               g[r][c+2] + g[r+1][c+1] + g[r+2][c] == s;
     }
 
     int numMagicSquaresInside(vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size();
-        int cnt = 0;
-
-        for(int i = 0; i <= m - 3; i++) {
-            for(int j = 0; j <= n - 3; j++) {
-                if(isMagicSquare(grid, i, j)) cnt++;
-            }
-        }
-        return cnt;
+        int res = 0;
+        for (int i = 0; i + 2 < grid.size(); i++)
+            for (int j = 0; j + 2 < grid[0].size(); j++)
+                if (isMagic(grid, i, j)) res++;
+        return res;
     }
 };
