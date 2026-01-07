59048115e10a74af3ad0e04201a986d0093fea82
suryansh-sahay <142234917+suryansh-sahay@users.noreply.github.com>
2026-01-08 00:22:24 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (13.61%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,27 +1,14 @@
 class Solution {
     double binaryExp(double x, long n) {
-        if (n == 0) {
-            return 1;
-        }
-
-        if (n < 0) {
-            n = -1 * n;
-            x = 1.0 / x;
-        }
-
-        double result = 1;
-        while (n != 0) {
-            if (n % 2 == 1) {
-                result = result * x;
-                n -= 1;
-            }
-            x = x * x;
-            n = n / 2;
-        }
-        return result;
+        if(n==0) return 1;
+        if(n%2==1) return x*binaryExp(x*x,n/2);
+        return binaryExp(x*x, n/2);
     }
 public:
     double myPow(double x, int n) {
-        return binaryExp(x, (long) n);
+        long N=n;
+        if(N<0) return (1.0/binaryExp(x, -1*N));
+
+        return binaryExp(x, (long) N);
     }
 };
