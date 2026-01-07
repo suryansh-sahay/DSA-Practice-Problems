89e935e5e937a161c2217d9b7486a2ffb6601364
suryansh-sahay <142234917+suryansh-sahay@users.noreply.github.com>
2026-01-07 23:33:24 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (5.23%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,4 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
 class Solution {
+    int recurse(int i, int sign, string s, long long res){
+        if(i>=s.size() || !isdigit(s[i])) return sign*res;
+        res=res*10+(s[i]-'0');
+        if(sign*res>INT_MAX) return INT_MAX;
+        else if(sign*res<INT_MIN) return INT_MIN;
+        return recurse(i+1, sign, s, res);
+    }
 public:
     int myAtoi(string s) {
         int i = 0, sign = 1;
@@ -9,13 +20,6 @@ public:
 
         if (s[i] == '-') { sign = -1; i++; }
         else if (s[i] == '+') i++;
-
-        while (i < s.size() && isdigit(s[i])) {
-            res = res * 10 + (s[i] - '0');
-            if (sign * res > INT_MAX) return INT_MAX;
-            if (sign * res < INT_MIN) return INT_MIN;
-            i++;
-        }
-        return (int)(sign * res);
+        return recurse(i, sign, s, res);
     }
 };
