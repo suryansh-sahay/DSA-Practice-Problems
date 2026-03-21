17e4906af77834ef38bfc7e60049eece8905d78e
suryansh-sahay <142234917+suryansh-sahay@users.noreply.github.com>
2026-03-22 03:52:25 +0530

    Time: 333 ms (75.30%) | Memory: 0B (100.00%) - LeetSync

--- 1452-restaurant-growth/restaurant-growth.sql
+++ 1452-restaurant-growth/restaurant-growth.sql
@@ -1,8 +1,24 @@
-SELECT a.visited_on AS visited_on, SUM(b.day_sum) AS amount,
-       ROUND(AVG(b.day_sum), 2) AS average_amount
-FROM
-  (SELECT visited_on, SUM(amount) AS day_sum FROM Customer GROUP BY visited_on ) a,
-  (SELECT visited_on, SUM(amount) AS day_sum FROM Customer GROUP BY visited_on ) b
-WHERE DATEDIFF(a.visited_on, b.visited_on) BETWEEN 0 AND 6
-GROUP BY a.visited_on
-HAVING COUNT(b.visited_on) = 7
+WITH daily AS (
+    SELECT 
+        visited_on,
+        SUM(amount) AS daily_amount
+    FROM Customer
+    GROUP BY visited_on
+)
+
+SELECT 
+    visited_on,
+    SUM(daily_amount) OVER (
+        ORDER BY visited_on
+        ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
+    ) AS amount,
+    
+    ROUND(
+        AVG(daily_amount) OVER (
+            ORDER BY visited_on
+            ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
+        ), 2
+    ) AS average_amount
+
+FROM daily
+LIMIT 1000000 OFFSET 6;
