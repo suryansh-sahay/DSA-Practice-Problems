7d2bb20858e3f449145295fd23c1433cfd9789ff
suryansh-sahay <142234917+suryansh-sahay@users.noreply.github.com>
2026-03-26 22:31:50 +0530

    Time: 394 ms (15.44%) | Memory: 0B (100.00%) - LeetSync

--- 1452-restaurant-growth/restaurant-growth.sql
+++ 1452-restaurant-growth/restaurant-growth.sql
@@ -1,24 +1,28 @@
-WITH daily AS (
-    SELECT 
-        visited_on,
-        SUM(amount) AS daily_amount
-    FROM Customer
+-- WITH daily AS (
+--     SELECT 
+--         visited_on,
+--         SUM(amount) AS daily_amount
+--     FROM Customer
+--     GROUP BY visited_on
+-- )
+
+WITH daily as(
+    SELECT visited_on, SUM(amount) as daily_amount
+    FROM Customer  
     GROUP BY visited_on
 )
-
 SELECT 
-    visited_on,
-    SUM(daily_amount) OVER (
-        ORDER BY visited_on
+    visited_on, 
+    SUM(daily_amount) OVER(
+        ORDER BY visited_on 
         ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
-    ) AS amount,
-    
-    ROUND(
-        AVG(daily_amount) OVER (
-            ORDER BY visited_on
-            ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
-        ), 2
-    ) AS average_amount
+    ) as amount,
 
+    ROUND(AVG(daily_amount) OVER(
+    ORDER BY visited_on 
+    ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
+    ), 2) as average_amount
+    
 FROM daily
-LIMIT 1000000 OFFSET 6;
+LIMIT 6,100000;
+
