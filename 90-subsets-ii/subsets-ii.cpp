313f849c154414c4db8c1946f4c5a48707d5fce5
suryansh-sahay <142234917+suryansh-sahay@users.noreply.github.com>
2026-03-30 17:52:30 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (64.74%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void func(int ind, vector<int>& arr, vector<vector<int>>& ans, vector<int>& nums){
+        ans.push_back(arr);
+        for(int i=ind; i<nums.size(); i++){
+            if(i!=ind && nums[i]==nums[i-1]) continue;
+            arr.push_back(nums[i]);
+            func(i+1, arr, ans, nums);
+            arr.pop_back();
+        }
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>> ans;
+        vector<int> arr;
+        sort(nums.begin(), nums.end());
+        func(0, arr, ans, nums);
+        return ans;
+    }
+};
+
