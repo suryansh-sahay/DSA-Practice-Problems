0e17965c2a4a5c4743dc5b56124c40f7ea4908a4
suryansh-sahay <142234917+suryansh-sahay@users.noreply.github.com>
2026-04-05 02:06:57 +0530

    Time: 7 ms (99.15%) | Memory: 35.2 MB (95.73%) - LeetSync

--- 2197-decode-the-slanted-ciphertext/decode-the-slanted-ciphertext.cpp
+++ 2197-decode-the-slanted-ciphertext/decode-the-slanted-ciphertext.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
-    string decodeCiphertext(string encodedText, int rows) {
-        int len = encodedText.length();
-        if (len == 0) return "";
-        
-        int cols = len / rows;
-        string ans = "";
-        
-        for (int j = 0; j < cols; j++) {
-            for (int r = 0, c = j; r < rows && c < cols; r++, c++) {
-                ans.push_back(encodedText[r * cols + c]);
+    string decodeCiphertext(string encoded, int rows) {
+        int n = encoded.length();
+        int cols = n / rows;
+
+        string original;
+        original.reserve(n);
+
+        int last_chr = -1;
+        for (int i = 0; i < cols; i++) {
+            for (int j = 0; j < rows; j++) {
+                if (i + j == cols) {
+                    original.resize(last_chr + 1); 
+                    return original;
+                }
+                int idx = j * cols + i + j;
+                if (encoded[idx] != ' ') last_chr = original.length();
+                original.push_back(encoded[idx]);
             }
         }
-        
-        while (!ans.empty() && ans.back() == ' ') {
-            ans.pop_back();
-        }
-        
-        return ans;
+        original.resize(last_chr + 1);
+        return original;
     }
 };
