5cd2e21abbceec5f759cad2572e30112db105bf6
suryansh-sahay <142234917+suryansh-sahay@users.noreply.github.com>
2026-04-09 02:33:47 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (90.85%) - LeetSync

---
+++ 301-remove-invalid-parentheses/remove-invalid-parentheses.cpp
@@ -0,0 +1,56 @@
+class Solution {
+private:
+    vector<string> res;
+
+public:
+    vector<string> removeInvalidParentheses(string s) {
+        helper(s, 0, 0, {'(', ')'});
+        return res;
+    }
+
+    void helper(string s, int left, int right, const vector<char>& para){
+        int len=s.length();
+        int counter=0;
+        while(right<len){
+            char ch= s[right];
+            if(ch== para[0]){
+                counter++;
+            }
+            else if(ch==para[1]){
+                counter--;
+            }
+            if(counter< 0){
+                break;
+            }
+            right++;
+
+        }
+
+        if(counter<0){
+            while(left<=right){
+                char ch= s[left];
+                if(ch!= para[1] || (left>0 && s[left]==s[left-1])){
+                    left++;
+                    continue;
+                }
+                // "(())()"
+                s.erase(left,1);
+                helper(s,left,right,para);
+                s.insert(s.begin()+left, para[1]);
+                left++;
+
+            }
+        }
+        else if(counter>0){
+            reverse(s.begin(),s.end());
+            helper(s,0,0,{')','(' });
+        }
+        else{
+            res.push_back(para[0]=='('? s : string(s.rbegin(), s.rend()));
+        }
+    }
+
+
+
+    
+};
