b939d1cfba5b29d012c40655caf742318c30975e
suryansh-sahay <142234917+suryansh-sahay@users.noreply.github.com>
2026-06-05 18:34:55 +0530

    Time: 0 ms (100.00%) | Memory: 196.4 MB (86.08%) - LeetSync

--- 3525-maximum-energy-boost-from-two-drinks/maximum-energy-boost-from-two-drinks.cpp
+++ 3525-maximum-energy-boost-from-two-drinks/maximum-energy-boost-from-two-drinks.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     long long maxEnergyBoost(vector<int>& energyDrinkA, vector<int>& energyDrinkB) {
-        long long dpA1=energyDrinkA[0], dpB1=energyDrinkB[0];
-        long long dpA0=dpA1, dpB0=dpB1;
+        long long dpA1 = 0, dpB1 = 0, dpA0=0, dpB0=0;
         long long dpA, dpB;
-        for(int i=1; i<energyDrinkA.size(); i++){
-
-            if(i<2) dpA=dpA1+energyDrinkA[i], dpB=dpB1+energyDrinkB[i], dpA1=dpA, dpB1=dpB;
-            else {
-            dpA=max(dpA1+energyDrinkA[i], dpB0+energyDrinkA[i]);
-            dpB=max(dpB1+energyDrinkB[i], dpA0+energyDrinkB[i]);
-            dpA0=dpA1, dpB0=dpB1;
-            dpA1=dpA, dpB1=dpB;
-            }
+        for (int i = 0; i < energyDrinkA.size(); i++) {
+            dpA = energyDrinkA[i] + max(dpA1, dpB0);
+            dpB = energyDrinkB[i] + max(dpB1, dpA0);
+            dpA0 = dpA1, dpB0 = dpB1;
+            dpA1 = dpA, dpB1 = dpB;
+        
         }
         return max(dpA, dpB);
     }
