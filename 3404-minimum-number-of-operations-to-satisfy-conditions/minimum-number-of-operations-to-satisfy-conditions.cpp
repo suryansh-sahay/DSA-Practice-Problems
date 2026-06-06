ea50a3b7611727f0a3d7b7de1f5c6f6c1e7ee72b
suryansh-sahay <142234917+suryansh-sahay@users.noreply.github.com>
2026-06-07 00:55:52 +0530

    Time: 21 ms (87.34%) | Memory: 118.1 MB (57.94%) - LeetSync

--- 3404-minimum-number-of-operations-to-satisfy-conditions/minimum-number-of-operations-to-satisfy-conditions.cpp
+++ 3404-minimum-number-of-operations-to-satisfy-conditions/minimum-number-of-operations-to-satisfy-conditions.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
-    
-    int solve(int col, int n, int m, int prev, vector<vector<int>> &freq, vector<vector<int>> &memo){
-        if(col == m) return 0;
-        
-        if(memo[col][prev+1] != -1) return memo[col][prev+1];
-        
-        int c=INT_MAX/2;
-        for(int num=0;num<=9;num++){
-            if(num != prev) c = min(c, n-freq[col][num] + solve(col+1, n, m, num, freq, memo));
-        }
-        
-        return memo[col][prev+1] = c;
-    }
-    
     int minimumOperations(vector<vector<int>>& grid) {
-        int n=grid.size(), m=grid[0].size();
-        
-        vector<vector<int>> freq(m, vector<int> (10,0));
-        for(int j=0;j<m;j++){
-            for(int i=0;i<n;i++){
-                freq[j][grid[i][j]]++;
+        int m= grid.size();
+        int n= grid[0].size();
+        vector<int>behind(10, 0);    
+
+        for(int i=n-1; i>=0; i--){  
+            vector<int>curr(10, INT_MAX);
+
+            vector<int>freq(10, 0);
+            for(int j=0; j< m; j++){
+                freq[grid[j][i]]++;
             }
+            for(int j=0; j< 10; j++){
+                for(int k=0; k< 10; k++){
+                    if(j==k) continue;
+                    curr[j]= min(curr[j] , (m- freq[j]) + behind[k]);
+                }
+            }
+            behind= curr;
         }
-        
-        vector<vector<int>> memo(m ,vector<int> (11, -1));
-        
-        return solve(0, n, m, -1, freq, memo);
+        int ans= INT_MAX;
+        for(int i=0; i< 10; i++){
+            ans= min(ans , behind[i]);
+
+        }
+        return ans;
     }
 };
