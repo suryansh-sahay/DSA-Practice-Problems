6c9b47f3eff372b486556d09c67157d123910349
suryansh-sahay <142234917+suryansh-sahay@users.noreply.github.com>
2026-06-24 13:30:32 +0530

    Time: 261 ms (81.40%) | Memory: 27.7 MB (79.07%) - LeetSync

--- 3964-number-of-zigzag-arrays-ii/number-of-zigzag-arrays-ii.cpp
+++ 3964-number-of-zigzag-arrays-ii/number-of-zigzag-arrays-ii.cpp
@@ -1,67 +1,72 @@
 class Solution {
-    const int MOD = 1e9 + 7;
-    using Matrix = vector<vector<long long>>;
+public:
+    const int mod = 1e9 + 7;
+    int zigZagArrays(int n, int l, int r) {
+        int k = r - l + 1;
+        if (n == 1) return k;
+        if (k == 1) return 0;
 
-    Matrix multiply(const Matrix& A, const Matrix& B) {
-        int sz = A.size();
-        Matrix C(sz, vector<long long>(sz, 0));
+        vector<long long> vec(k, 0);
+        iota(vec.begin(), vec.end(), 0);
 
-        for (int i = 0; i < sz; i++) {
-            for (int k = 0; k < sz; k++) {
-                if (!A[i][k]) continue;
-                for (int j = 0; j < sz; j++) {
-                    C[i][j] = (C[i][j] + A[i][k] * B[k][j]) % MOD;
+        vector<vector<long long>> mat(k, vector<long long>(k, 0));
+        for (int i = 0; i < k; ++i) {
+            int down = k - 1 - i;
+            for (int j = down + 1; j < k; ++j) {
+                mat[i][j] = 1;
+            }
+        }
+
+        auto trans = mulPow(mat, n - 2);
+        auto out = vecMul(trans, vec);
+
+        long long ans = 0;
+        for (long long val : out) {
+            ans = (ans + val) % mod;
+        }
+        return ans * 2 % mod;
+    }
+
+    vector<vector<long long>> mulPow(vector<vector<long long>>& a, int exp) {
+        int n = a.size();
+        vector<vector<long long>> res(n, vector<long long>(n, 0));
+        for (int i = 0; i < n; ++i) {
+            res[i][i] = 1;
+        }
+        while (exp) {
+            if (exp % 2 == 1) {
+                res = matMul(res, a);
+            }
+            a = matMul(a, a);
+            exp /= 2;
+        }
+        return res;
+    }
+
+    vector<vector<long long>> matMul(vector<vector<long long>>& a, 
+                                        vector<vector<long long>>& b) {
+        int n = a.size();
+        vector<vector<long long>> res(n, vector<long long>(n, 0));
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                for (int k = 0; k < n; ++k) {
+                    res[i][j] = (res[i][j] + a[i][k] * b[k][j]) % mod;
                 }
             }
         }
-
-        return C;
+        return res;
     }
 
-public:
-    int zigZagArrays(int n, int l, int r) {
-        if (n == 1) return r - l + 1;
-
-        int k = r - l + 1;
-        int sz = 2 * k;
-
-        Matrix M(sz, vector<long long>(sz, 0));
-
-        for (int i = 0; i < k; i++) {
-            for (int j = 0; j < i; j++) {
-                M[i][k + j] = 1;
-            }
-
-            for (int j = i + 1; j < k; j++) {
-                M[k + i][j] = 1;
+    vector<long long> vecMul(vector<vector<long long>>& a, 
+                                        vector<long long>& b) {
+        int n = a.size();
+        vector<long long> res(n, 0);
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                res[i] = (res[i] + a[i][j] * b[j]) % mod;
             }
         }
+        return res;
 
-        Matrix res(sz, vector<long long>(sz, 0));
-
-        for (int i = 0; i < sz; i++) {
-            res[i][i] = 1;
-        }
-
-        long long p = n - 1;
-
-        while (p > 0) {
-            if (p % 2 == 1) {
-                res = multiply(res, M);
-            }
-
-            M = multiply(M, M);
-            p /= 2;
-        }
-
-        long long ans = 0;
-
-        for (int i = 0; i < sz; i++) {
-            for (int j = 0; j < sz; j++) {
-                ans = (ans + res[i][j]) % MOD;
-            }
-        }
-
-        return (int)ans;
     }
 };
