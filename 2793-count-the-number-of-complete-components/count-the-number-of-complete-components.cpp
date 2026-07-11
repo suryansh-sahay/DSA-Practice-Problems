f4787058edd251911f0f81620d297d23cd0802b4
suryansh-sahay <142234917+suryansh-sahay@users.noreply.github.com>
2026-07-11 19:33:25 +0530

    Time: 40 ms (69.87%) | Memory: 130.1 MB (63.27%) - LeetSync

--- 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
@@ -1,28 +1,35 @@
 class Solution {
 public:
-    void dfs(int node, vector<vector<int>>& adj, vector<int>& vis, int& nodes, int& degSum) {
-        vis[node]=1;
-        nodes++;
-        degSum+=adj[node].size();
-        for(int nei:adj[node]){
-            if(!vis[nei])
-                dfs(nei,adj,vis,nodes,degSum);
-        }
+    void dfs(int u, vector<vector<int>>& adj, vector<bool>& vis, vector<int>& comp) {
+        vis[u] = true;
+        comp.push_back(u);
+        for (int v : adj[u])
+            if (!vis[v]) dfs(v, adj, vis, comp);
     }
+
     int countCompleteComponents(int n, vector<vector<int>>& edges) {
         vector<vector<int>> adj(n);
-        for(auto &e:edges){
+        for (auto& e : edges) {
             adj[e[0]].push_back(e[1]);
             adj[e[1]].push_back(e[0]);
         }
-        vector<int> vis(n,0);
-        int ans=0;
-        for(int i=0;i<n;i++){
-            if(!vis[i]){
-                int nodes=0,degSum=0;
-                dfs(i,adj,vis,nodes,degSum);
-                int edgeCount=degSum/2;
-                if(edgeCount==nodes*(nodes-1)/2) ans++;
+
+        vector<bool> vis(n, false);
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (!vis[i]) {
+                vector<int> comp;
+                dfs(i, adj, vis, comp);
+
+                bool isComplete = true;
+                for (int u : comp) {
+                    if (adj[u].size() != comp.size() - 1) {
+                        isComplete = false;
+                        break;
+                    }
+                }
+                if (isComplete) ans++;
             }
         }
         return ans;
