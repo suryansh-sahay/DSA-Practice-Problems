af0984d8e77a36d8a044dcd835d7dba9d082c27a
Suryansh Sahay <142234917+suryansh-sahay@users.noreply.github.com>
2026-07-19 19:30:01 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (50.00%) - LeetSync

--- 4364-maximum-value-of-an-alternating-sequence/maximum-value-of-an-alternating-sequence.cpp
+++ 4364-maximum-value-of-an-alternating-sequence/maximum-value-of-an-alternating-sequence.cpp
@@ -1,11 +1,8 @@
 class Solution {
 public:
-    long long maximumValue(int n, int s, int m) {
-        long long ans=s;
-        long long k=n/2;
-        if(k>0) ans=max(ans,1LL*s+k*m-(k-1));
-        long long t=(n-1)/2;
-        ans=max(ans,1LL*s+t*(m-1));
-        return ans;
+    long long maximumValue(int n, int s, int m) {        
+        if(n==1) return s;
+        long long k=1LL*(n/2);
+        return 1LL*s+(k)*m-(k-1);
     }
 };
