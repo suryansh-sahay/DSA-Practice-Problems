904869bc7327b7ec1870bc95cbba85be66ff67ad
Suryansh Sahay <142234917+suryansh-sahay@users.noreply.github.com>
2026-08-18 15:56:44 +0530

    Time: 64 ms (12.20%) | Memory: 30.1 MB (6.46%) - LeetSync

--- 149-max-points-on-a-line/max-points-on-a-line.cpp
+++ 149-max-points-on-a-line/max-points-on-a-line.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     int maxPoints(vector<vector<int>>& points) {
-        int n = points.size();
-        if(n<=2)
-            return n;
+        int n=points.size();
+        if(n<=2) return n;
         
-        int maxi = 2;
-        for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                int count=2;
-                for(int k=0; k<n; k++){
-                    if(k!=i && k!=j){
-                        if((points[j][1]-points[i][1])*(points[j][0]-points[k][0])==(points[j][1]-points[k][1])*(points[j][0]-points[i][0]))
-                            count++;
-                    }
+        int ans=0;
+        for(auto pt1: points){
+            double x1= pt1[0], y1=pt1[1];
+            unordered_map<double, int> slpe;
+
+            for(auto pt2: points){
+                if(pt1!=pt2){
+                    double slp;
+                    double x2= pt2[0], y2=pt2[1];
+                    if(x2==x1) slp=INT_MAX;
+                    else slp=(y2-y1)/(x2-x1);
+                    slpe[slp]++;
+                    ans= max(ans, slpe[slp]);
                 }
-                maxi = max(maxi,count);
             }
         }
-        return maxi;
+        return ans+1;
     }
 };
