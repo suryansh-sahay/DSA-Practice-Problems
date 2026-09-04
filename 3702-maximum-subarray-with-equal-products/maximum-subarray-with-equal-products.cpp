6b0a240bf8322cd81f4a6a9e057e5ebbe45d2a3b
Suryansh Sahay <142234917+suryansh-sahay@users.noreply.github.com>
2026-09-05 02:13:49 +0530

    Time: 0 ms (100.00%) | Memory: 28 MB (86.81%) - LeetSync

--- 3702-maximum-subarray-with-equal-products/maximum-subarray-with-equal-products.cpp
+++ 3702-maximum-subarray-with-equal-products/maximum-subarray-with-equal-products.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
     int maxLength(vector<int>& nums) {
-        int cnt=2, n=nums.size(), M=5*7*8*9;
-        for(int i=0; i<n; i++){
-            int x=nums[i];
-            int P=x, L=x, G=x, y;
-            for(int j=i+1; j<n && P<=M; j++){
-                y=nums[j];
-                P*=y;
-                G=gcd(G, y);
-                L=lcm(L, y);
-                if (P==G*L) cnt=max(cnt, j-i+1);
-                else break;
+        int ans = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            long long prdt = 1;
+            long long lcmm = 1;
+            int gcdd = 0;
+
+            for (int j = i; j < nums.size(); j++) {
+                prdt *= nums[j];
+                if (prdt > 36288000) break;
+
+                lcmm = lcm(lcmm, (long long)nums[j]);
+                gcdd = gcd(gcdd, nums[j]);
+
+                if (prdt == lcmm * gcdd) {
+                    ans = max(ans, j - i + 1);
+                }
             }
         }
-        return cnt;
+
+        return ans;
     }
 };
