a09498942d3328e0721753eee3254fa63a127fc1
Suryansh Sahay <142234917+suryansh-sahay@users.noreply.github.com>
2026-09-04 20:43:14 +0530

    Time: 0 ms (100.00%) | Memory: 30.9 MB (73.61%) - LeetSync

--- 4284-smallest-stable-index-i/smallest-stable-index-i.cpp
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.cpp
@@ -4,10 +4,9 @@ public:
         int mini[100];
 
         int n = nums.size();
-        mini[n - 1] = nums[n - 1];
+        mini[n - 1] = nums.back();
 
-        for (int i = n - 2; i >= 0; i--)
-            mini[i] = min(nums[i], mini[i + 1]);
+        for (int i = n - 2; i >= 0; i--) mini[i] = min(nums[i], mini[i + 1]);
 
         int maxi = 0;
         for (int i = 0; i < n; i++) {
