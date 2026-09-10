4243e6e311331371e6453693ab01fa6c508ec39d
Suryansh Sahay <142234917+suryansh-sahay@users.noreply.github.com>
2026-09-11 03:38:24 +0530

    Time: 0 ms (100.00%) | Memory: 15.8 MB (64.58%) - LeetSync

--- 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.cpp
+++ 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.cpp
@@ -1,22 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    pair<int, int> trav(TreeNode* root, int& count) {
-        if(root == nullptr) return {0, 0};
+    int ans=0;
+    pair<int, int> helper(TreeNode* root){
+        if(root==NULL) return {0,0};
 
-        auto [leftSum, leftCount] = trav(root->left, count);
-        auto [rightSum, rightCount] = trav(root->right, count);
+        auto left = helper(root->left);
+        auto right = helper(root->right);
 
-        int subtreeSum = leftSum + rightSum + root->val;
-        int subtreeCount = leftCount + rightCount + 1;
+        int sum=left.first+right.first+root->val;
+        int count=left.second+right.second+1;
 
-        if(subtreeSum / subtreeCount == root->val) count++;
+        if(sum/count==root->val){
+            ans++;
+        }
 
-        return {subtreeSum, subtreeCount};
+        return {sum, count};
     }
 
     int averageOfSubtree(TreeNode* root) {
-        int count = 0;
-        trav(root, count);
-        return count;
+        helper(root);
+        return ans;
     }
 };
